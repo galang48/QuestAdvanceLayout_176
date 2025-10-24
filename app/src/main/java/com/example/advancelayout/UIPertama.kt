b5a411ee65e499bceb48f65812f5069d309e781c
galang48 <galangyudha1945@gmail.com>
2025-10-24 09:33:44 +0700

    menambahkan jarak vertikal setinggi 25dp`

--- app/src/main/java/com/example/advancelayout/UIPertama.kt
+++ app/src/main/java/com/example/advancelayout/UIPertama.kt
@@ -1,7 +1,9 @@
 package com.example.advancelayout
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -29,6 +31,7 @@ fun ActivitasPertama(modifier: Modifier){
                 stringResource(id= R.string.univ),
                 fontSize = 22.sp
             )
+            Spacer(modifier = Modifier.height(25.dp))
     }
 
 }
